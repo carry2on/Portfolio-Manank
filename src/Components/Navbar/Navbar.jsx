82632d000fbbb7c5009af438cd97adccd362d5e4
carry2on <manankpandya307@gmail.com>
2025-05-19 21:56:59 +0530

    Update Navbar.jsx

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,62 +1,63 @@
 import React, { useState } from "react";
 import { RiCloseLine, RiMenu2Line } from "@remixicon/react";
 import Logo from "../../assets/initial-mp-logo-star-shield-symbol-with-simple-design-vector.jpg"
+
 const Navbar = () => {
   const [menu, openMenu] = useState(false);
   const [showMenu, setShowmenu] = useState(true);
-  return (
-    <nav className=" bg-[#404F8B] flex flex-wrap justify-between md:items-center text-white px-10 md:px-20">
-      <div className="w-24 h-24" >
-        <img src={Logo} alt="" srcset="" /></div>
-      
+  
+  const toggleMenu = () => {
+    openMenu(!menu);
+    setShowmenu(!showMenu);
+  };
 
+  return (
+    <nav className="bg-[#404F8B] flex flex-wrap justify-between md:items-center text-white px-10 md:px-20">
+      <div className="w-24 h-24">
+        <img src={Logo} alt="" srcset="" />
+      </div>
+      
       <ul
         className={`${
           menu ? "block" : "hidden"
-        }     mx-24 p-y2 mt-4 font-semibold md:mt-5 bg-black px-2 rounded-xl bg-opacity-30 md:border-none text-center md:bg-transparent md:static md:mx-0 md:flex gap-6`}
+        } mx-24 p-y2 mt-4 font-semibold md:mt-5 bg-black px-2 rounded-xl bg-opacity-30 md:border-none text-center md:bg-transparent md:static md:mx-0 md:flex gap-6`}
       >
-        
         <a href="#About">
-          <li className=" hover:text-orange-300 text-lg  transition-all duration-300 p-1 md:p-0">
+          <li className="hover:text-orange-300 text-lg transition-all duration-300 p-1 md:p-0">
             About
           </li>
         </a>
         <a href="#Experience">
-          <li className=" hover:text-orange-300 text-lg transition-all duration-300 p-1 md:p-0">
+          <li className="hover:text-orange-300 text-lg transition-all duration-300 p-1 md:p-0">
             Experience
           </li>
         </a>
         <a href="#Projects">
-          <li className="  hover:text-orange-300 text-lg transition-all duration-300 p-1 md:p-0">
+          <li className="hover:text-orange-300 text-lg transition-all duration-300 p-1 md:p-0">
             Projects
           </li>
         </a>
         <a href="#Footer">
-          <li className="  hover:text-orange-300 text-lg transition-all duration-300 p-1 md:p-0">
+          <li className="hover:text-orange-300 text-lg transition-all duration-300 p-1 md:p-0">
             Contact
           </li>
         </a>
-      </ul> 
-
-
+      </ul>
+      
       {showMenu ? (
         <RiMenu2Line
           size={30}
           className="md:hidden absolute right-10 top-6 transition-all duration-300"
-          onClick={() => {
-            openMenu(!menu);
-            setShowmenu(!showMenu);
-          }}
+          onClick={toggleMenu}
         />
       ) : (
         <RiCloseLine
           size={30}
           className="md:hidden absolute right-10 top-6 transition-all duration-300"
+          onClick={toggleMenu}
         />
       )}
-      
     </nav>
-
   );
 };
 
